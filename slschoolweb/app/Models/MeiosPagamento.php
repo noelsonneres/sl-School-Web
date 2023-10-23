40ed11a445972f8666a2f297cbb66888fd2e9e48
noelsonneres <noelsonneres@gmail.com>
2023-10-23 20:26:54 -0300

    Cadastro dos Meios de Pagamentos

---
+++ slschoolweb/app/Models/MeiosPagamento.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MeiosPagamento extends Model
+{
+    use HasFactory;
+}
