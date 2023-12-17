bf8a2d9064597edc5b101aa8eeef13652edd1f6c
noelsonneres <noelsonneres@gmail.com>
2023-12-16 21:19:38 -0300

    Processo de contas a pagar

--- slschoolweb/app/Http/Controllers/ContasPagarController.php
+++ slschoolweb/app/Http/Controllers/ContasPagarController.php
@@ -30,41 +30,30 @@ class ContasPagarController extends Controller
         return view(self::PATH.'planoContasCreate', ['planoContas'=>$planoContas]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+
+        $conta = $this->contas;
+
+//        Continuar desta parte em diante
+
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
--- slschoolweb/resources/views/screens/contasPagar/planoContasCreate.blade.php
+++ slschoolweb/resources/views/screens/contasPagar/planoContasCreate.blade.php
@@ -22,18 +22,18 @@
 
         <div class="card p-5">
 
-            <form action="{{ route('salas.store') }}" method="post">
+            <form action="{{ route('contas_pagar.store') }}" method="post">
 
                 @csrf
 
                 <div class="row">
 
-                    <div class="col-md-8 mb-6">
+                    <div class="col-md-8 mb-4">
                         <label for="conta" class="form-label lblCaption">Conta</label>
                         <input type="text" class="form-control" name="conta" id="conta" maxlength="50"
-                               autofocus required value="{{old('sala')}}">
+                               autofocus required>
                     </div>
-                    <div class="col-md-4 mb-6">
+                    <div class="col-md-4 mb-4">
                         <label for="tipo" class="form-label lblCaption">Plano de contas</label>
                         <select class="form-control" name="tipo" id="tipo" required>
                             <option value="">Selecione o plano de contas</option>
@@ -44,7 +44,70 @@
 
                         </select>
                     </div>
+                </div>
 
+                <div class="mb-4">
+                    <label for="descricao" class="form-label lblCaption">Descrição</label>
+                    <input type="text" class="form-control" name="descricao" id="descricao" maxlength="100">
+                </div>
+
+                <div class="row">
+
+                    <div class="col-md-3 mb-4">
+                        <label for="valor" class="form-label lblCaption">Valor</label>
+                        <input type="number" class="form-control" name="valor" id="valor" step="0.01" min="0.01" required>
+                    </div>
+
+                    <div class="col-md-3 mb-4">
+                        <label for="vencimento" class="form-label lblCaption">Vencimento</label>
+                        <input type="date" class="form-control" name="vencimento" id="vencimento" required>
+                    </div>
+
+                    <div class="col-md-3 mb-4">
+                        <label for="juros" class="form-label lblCaption">Juros %</label>
+                        <input type="number" class="form-control" name="juros" id="juros" step="0.01" min="0.01">
+                    </div>
+
+                    <div class="col-md-3 mb-4">
+                        <label for="multa" class="form-label lblCaption">Multa</label>
+                        <input type="number" class="form-control" name="multa" id="multa" step="0.01" min="0.01">
+                    </div>
+                </div>
+
+                <div class="row mb-4">
+
+                    <div class="col-md-3">
+                        <label for="desconto" class="form-label lblCaption">Desconto</label>
+                        <input type="number" class="form-control" name="desconto" id="desconto" step="0.01" min="0.01">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="acrescimo" class="form-label lblCaption">Acréscimo</label>
+                        <input type="number" class="form-control" name="acrescimo" id="acrescimo" step="0.01" min="0.01">
+                    </div>
+
+                    <div class="col-md-2">
+                        <label for="dtPagamento" class="form-label lblCaption">DT. Pagamento</label>
+                        <input type="date" class="form-control" name="dtPagamento" id="dtPagamento">
+                    </div>
+
+                    <div class="col-md-1">
+                        <label for="pago" class="form-label lblCaption">Pago ?</label>
+                        <select class="form-control" name="pago" id="pago" >
+                            <option value="sim">sim</option>
+                            <option value="nao" >nao</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="total" class="form-label lblCaption">Total</label>
+                        <input type="number" class="form-control" name="total" id="total">
+                    </div>
+                </div>
+
+                <div class="mb-4">
+                    <label for="obs" class="form-label lblCaption">Observação</label>
+                    <input type="text" class="form-control" name="obs" id="obs" maxlength="255">
                 </div>
 
                 <div>
