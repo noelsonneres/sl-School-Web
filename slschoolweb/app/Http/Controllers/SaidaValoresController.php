3c7db10dc907d2049cc3e8363c0ce8a09301bdcd
noelsonneres <noelsonneres@gmail.com>
2023-12-20 14:28:01 -0300

    Processo de Saídas de Valores

--- slschoolweb/app/Http/Controllers/SaidaValoresController.php
+++ slschoolweb/app/Http/Controllers/SaidaValoresController.php
@@ -20,24 +20,66 @@ class SaidaValoresController extends Controller
     {
 
         $saidas = $this->saidas->paginate();
-        return view(self::PATH.'saidaValoresShow');
+        return view(self::PATH . 'saidaValoresShow', ['saidaValores' => $saidas]);
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+
+        return view(self::PATH . 'saidaValoresCreate');
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+
+        $saida = $this->saidas;
+
+        $request->validate([
+            'motivo' => 'required|min:3|max:50',
+            'data' => 'required',
+            'hora' => 'required',
+            'valor' => 'required',
+        ], [
+            'motivo.required' => 'Informe um valor para o campo Motivo',
+            'motivo.min' => 'O motivo da motivo deve ter mais de três caracteres',
+            'motivo.max' => 'O motivo da motivo deve ter mens de 50 caracteres',
+            'data.required' => 'Selecione uma data valida para a saída',
+            'hora.required' => 'Informe um horário valido',
+            'valor.required' => 'Digite o valor da saída',
+        ]);
+
+        $motivo = $request->old('motivo');
+        $data = $request->old('data');
+        $hora = $request->old('hora');
+        $valor = $request->old('valor');
+
+        $msg = '';
+
+        try {
+
+            if ($this->verificarCaixa() === true) {
+                $saida->motivo = $request->input('motivo');
+                $saida->data = $request->input('data');
+                $saida->hora = $request->input('hora');
+                $saida->valor = $request->input('valor');
+                $saida->observacao = $request->input('obs');
+
+                $saida->save();
+
+                $msg = 'SUCESSO! Saída efetuada com sucesso!';
+            }else{
+                $msg = 'ATENÇÃO! Não há saldo suficiente para esta saída!';
+            }
+
+        } catch (\Throwable $th) {
+            $msg = 'ERRO! Não foi possível realizar a Saída dos valores: ' . $th->getMessage();
+        }
+
+        $saidas = $this->saidas->paginate();
+        return view(self::PATH . 'saidaValoresShow', ['saidaValores' => $saidas])
+            ->with('msg', $msg);
     }
 
     /**
@@ -69,6 +111,35 @@ class SaidaValoresController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+
+        $saida = $this->saidas->find($id);
+        $msg = '';
+
+        if ($saida->count() >= 1){
+
+            try {
+                $saida->delete();
+                $msg = 'SUCESSO! Saída removida com sucesso!';
+            }catch (\Throwable $th){
+                $msg = 'ERRO! Não foi possível deletar a saida: '.$th->getMessage();
+            }
+
+        }else{
+            $msg = 'ATENÇÃO! Não foi possível localizar a saída!';
+        }
+
+        $saidas = $this->saidas->paginate();
+        return view(self::PATH . 'saidaValoresShow', ['saidaValores' => $saidas])
+            ->with('msg', $msg);
+
     }
+
+    
+
+    private function verificarCaixa()
+    {
+        return true;
+//        Veirificar se há saldo sufuciente no caixa para o lançamnto da saída
+    }
+
 }
