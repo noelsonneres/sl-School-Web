c54cab0869ac1abd5646b90eaab6ba381680de81
noelsonneres <noelsonneres@gmail.com>
2024-02-02 16:25:23 -0300

    Relatório Mensalidades Atrasadas

--- slschoolweb/app/Http/Controllers/RelatorioMensalidadesAtrasadasController.php
+++ slschoolweb/app/Http/Controllers/RelatorioMensalidadesAtrasadasController.php
@@ -20,7 +20,10 @@ class RelatorioMensalidadesAtrasadasController extends Controller
     public function index()
     {
         $currentDate = Carbon::now()->format('Y-m-d');
-        $mensalidades = $this->mensalidade->where('vencimento', '<=', $currentDate)->paginate();
+        $mensalidades = $this->mensalidade
+            ->where('vencimento', '<=', $currentDate)
+            ->where('pago', '=', 'nao')
+            ->paginate();
         return view(self::PATH . 'relMensalidadesAtrasadas', ['mensalidades' => $mensalidades]);
     }
 
@@ -28,26 +31,39 @@ class RelatorioMensalidadesAtrasadasController extends Controller
     {
 
         $request->validate([
-            'dt1'=>'required',
-            'dt2'=>'required',
-        ],[
-            'dt1.required'=>'Informe a primeira data de vencimento para localizar as mensalidades',
-            'dt2.required'=>'Informe a segunda data de vencimento para localizar as mensalidades',
+            'dt1' => 'required',
+            'dt2' => 'required',
+        ], [
+            'dt1.required' => 'Informe a primeira data de vencimento para localizar as mensalidades',
+            'dt2.required' => 'Informe a segunda data de vencimento para localizar as mensalidades',
         ]);
+
+        $dt1 = $request->old('dt1');
 
         $dt1 = $request->input('dt1');
         $dt2 = $request->input('dt2');
 
-        $mensalidades = $this->mensalidade->whereBetween('vencimento', [$dt1, $dt2])->paginate();
+        $mensalidades = $this->mensalidade->whereBetween('vencimento', [$dt1, $dt2])->where('pago', '=', 'nao')->paginate();
         return view(self::PATH . 'relMensalidadesAtrasadas', ['mensalidades' => $mensalidades]);
-
     }
 
     public function localizarApenasAtrasadas(Request $request)
     {
+        $currentDate = Carbon::now()->format('Y-m-d');
+        $mensalidades = $this->mensalidade->where('vencimento', '<=', $currentDate)->where('pago', '=', 'nao')->paginate();
+        return view(self::PATH . 'relMensalidadesAtrasadas', ['mensalidades' => $mensalidades]);
     }
 
     public function localizarPorMatrícula(Request $request)
     {
+
+        $request->validate([
+            'dt1'=>'required',
+            'dt2'=>'required',
+            'matricula'=>'required',
+        ],[
+            'dt1.required'=>'Digite a primeira data para pesquisar'
+        ]);
+
     }
 }
--- slschoolweb/resources/views/screens/relatorios/mensalidade/relMensalidadesAtrasadas.blade.php
+++ slschoolweb/resources/views/screens/relatorios/mensalidade/relMensalidadesAtrasadas.blade.php
@@ -23,18 +23,18 @@
 
         <div class="row">
 
-            <div class="col-6 border">
+            <div class="col-5 border">
                 <form action="/rel_atrasadas_loc_data" method="get">
 
                     <div class="row">
                         <div class="col-md-5 mb-2 mt-2">
                             <label for="dt1" class="form-label">Início</label>
-                            <input type="date" class="form-control" name="dt1" id="dt1">
+                            <input type="date" class="form-control" name="dt1" id="dt1" required>
                         </div>
 
                         <div class="col-md-5 mb-2 mt-2">
                             <label for="dt2" class="form-label">Término</label>
-                            <input type="date" class="form-control" name="dt2" id="dt2">
+                            <input type="date" class="form-control" name="dt2" id="dt2" required>
                         </div>
 
                         <div class="col-md-2 mt-2 p-2">
@@ -51,14 +51,26 @@
                 </form>
             </div>
 
-            <div class="col-md-5 border p-2 ms-2">
-                <form action="#" method="get">
+            <div class="col-md-6 border p-2 ms-1">
+                <form action="/rel_atrasadas_loc_atrasadas" method="get">
 
-                    <div class="mt-4 ps-3">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                        <label class="form-check-label h4" for="flexCheckChecked">
-                            Todas as mensalidades atrasadas
-                        </label>
+                    <div class="row">
+
+                        <div class="col-md-7 mb-2 mt-4">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                            <label class="form-check-label h4" for="flexCheckChecked">
+                                Todas as mensalidades atrasadas
+                            </label>
+                        </div>
+
+                        <div class="col-md-2">
+                            <label for=""></label>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success btn">
+                                    <i class="bi bi-search"></i>
+                                </button>
+                            </div>
+                        </div>
                     </div>
 
                 </form>
@@ -73,17 +85,17 @@
                 <div class="row border mt-2">
                     <div class="col-md-3 mb-2 mt-2">
                         <label for="dt1" class="form-label">Início</label>
-                        <input type="date" class="form-control" name="dt1" id="dt1">
+                        <input type="date" class="form-control" name="dt1" id="dt1" required>
                     </div>
 
                     <div class="col-md-3 mb-2 mt-2">
                         <label for="dt2" class="form-label">Término</label>
-                        <input type="date" class="form-control" name="dt2" id="dt2">
+                        <input type="date" class="form-control" name="dt2" id="dt2" required>
                     </div>
 
                     <div class="col-md-4 mb-2 mt-2">
                         <label for="matricula" class="form-label">Matrícula</label>
-                        <input type="text" class="form-control" name="matricula" id="matricula">
+                        <input type="number" class="form-control" name="matricula" id="matricula" required>
                     </div>
 
                     <div class="col-md-2 mt-2 p-2">
@@ -97,8 +109,8 @@
 
                 </div>
 
-            </form>            
-            
+            </form>
+
         </div>
 
         <hr>
